2dc95572f1306b74937911817666f6484bd03db0
Peter Gazdik <peter.gazdik@gmail.com>
2024-05-18 14:13:25 +0200

    Adding support for jakarta.servlet

---
+++ servlet-tools-api/src/dev/hiconic/servlet/api/HttpFilter.java
@@ -0,0 +1,40 @@
+// ============================================================================
+// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2022
+// 
+// This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this library; See http://www.gnu.org/licenses/.
+// ============================================================================
+package dev.hiconic.servlet.api;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface HttpFilter extends Filter {
+	@Override default void init(FilterConfig filterConfig) throws ServletException { /* noop */ }
+	@Override default void destroy() { /* noop */ }
+
+	@Override
+	default void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		HttpServletResponse httpResponse = (HttpServletResponse)response;
+		
+		doFilter(httpRequest, httpResponse, chain);
+	}
+
+	void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException;
+}
